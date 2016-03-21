4433344a7be02ec63ec2a35c6fa4668a3337ac62
She16 <She_16@i.ua>
2016-03-21 11:17:14 +0200

    Added files via upload

---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://kaminus.com.ua/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://kaminus.com.ua/');
+
+// DIR
+define('DIR_APPLICATION', '/home/kamin702/public_html/catalog/');
+define('DIR_SYSTEM', '/home/kamin702/public_html/system/');
+define('DIR_DATABASE', '/home/kamin702/public_html/system/database/');
+define('DIR_LANGUAGE', '/home/kamin702/public_html/catalog/language/');
+define('DIR_TEMPLATE', '/home/kamin702/public_html/catalog/view/theme/');
+define('DIR_CONFIG', '/home/kamin702/public_html/system/config/');
+define('DIR_IMAGE', '/home/kamin702/public_html/image/');
+define('DIR_CACHE', '/home/kamin702/public_html/system/cache/');
+define('DIR_DOWNLOAD', '/home/kamin702/public_html/download/');
+define('DIR_LOGS', '/home/kamin702/public_html/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'kamin702_John');
+define('DB_PASSWORD', '*.TKu91lRi~k');
+define('DB_DATABASE', 'kamin702_01');
+define('DB_PREFIX', 'oc_');
+?>
